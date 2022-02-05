f10d71bfc0e873e958e863db1cea996641e65a2e
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-05 14:31:33 +0530

    Initial Start

---
+++ newsapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  // can make variables like c='Palash' and then call below using {this.c}
+  render() {
+    return (
+      <div>
+        This is First Class Based Component
+      </div>
+    )
+  }
+}

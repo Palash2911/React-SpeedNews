2e8c0160999861963d57b8b4c45cb136f70e8be1
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-07 17:08:54 +0530

    Added Constructor and New Json File

--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
+    // when props called title and description will be assigned those value
+    let {title, description, imgurl, newsurl} = this.props
+
     return (
-      <div>
-        News item
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imgurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-sm btn-success">Read More</a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;

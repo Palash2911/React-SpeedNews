2c26b6b5ca91eb0fcb30001836c0b85848e747ec
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-08 19:48:16 +0530

    Updated

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -9,6 +9,8 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+
+  pagesize=6
   // can make variables like c='Palash' and then call below using {this.c}
   render() {
     return (
@@ -18,21 +20,21 @@ export default class App extends Component {
           {/* You can pass country also but i dont want to */}
             <Routes>
             {/* Use exact if making big website so when user click / only that part opens */}
-              <Route exact path="/" element={ <Newscompo key="general" srcolor="danger" pagesize="6" category="general"/>}>
+              <Route exact path="/" element={ <Newscompo key="general" srcolor="danger" pagesize={this.pagesize} category="general"/>}>
                 </Route>
-              <Route exact path="/sports" element={ <Newscompo key="sports" srcolor="warning" pagesize="6" category="sports"/>}>
+              <Route exact path="/sports" element={ <Newscompo key="sports" srcolor="warning" pagesize={this.pagesize} category="sports"/>}>
                 </Route>
-              <Route exact path="/business" element={ <Newscompo key="business" srcolor="success" pagesize="6" category="business"/>}>
+              <Route exact path="/business" element={ <Newscompo key="business" srcolor="success" pagesize={this.pagesize} category="business"/>}>
                 </Route>
-              <Route exact path="/technology" element={ <Newscompo key="technology" srcolor="dark" pagesize="6" category="technology"/>}>
+              <Route exact path="/technology" element={ <Newscompo key="technology" srcolor="dark" pagesize={this.pagesize} category="technology"/>}>
                 </Route>
-              <Route exact path="/general" element={ <Newscompo key="general" srcolor="danger" pagesize="6" category="general"/>}>
+              <Route exact path="/general" element={ <Newscompo key="general" srcolor="danger" pagesize={this.pagesize} category="general"/>}>
                 </Route>
-              <Route exact path="/health" element={ <Newscompo key="health" srcolor="info" pagesize="6" category="health"/>}>
+              <Route exact path="/health" element={ <Newscompo key="health" srcolor="info" pagesize={this.pagesize} category="health"/>}>
                 </Route>
-              <Route exact path="/entertainment" element={ <Newscompo key="entertainment" srcolor="secondary" pagesize="6" category="entertainment"/>}>
+              <Route exact path="/entertainment" element={ <Newscompo key="entertainment" srcolor="secondary" pagesize={this.pagesize} category="entertainment"/>}>
                 </Route>
-              <Route exact path="/science" element={ <Newscompo key="science" srcolor="primary" pagesize="6" category="science"/>}>
+              <Route exact path="/science" element={ <Newscompo key="science" srcolor="primary" pagesize={this.pagesize} category="science"/>}>
                 </Route>
           </Routes>
         </Router>
--- newsapp/src/components/Newscompo.js
+++ newsapp/src/components/Newscompo.js
@@ -1,93 +1,134 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
-import Spinner  from './Spinner';
-import PropTypes from 'prop-types'
-
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class Newscompo extends Component {
-  
-  // Prop-Types 
+  // Prop-Types
   static defaultProps = {
-      pagesize: 6,
-      category: 'general',
-      srcolor: "danger"
-  }
+    pagesize: 6,
+    category: "general",
+    srcolor: "danger",
+  };
 
   static props = {
-      pagesize: PropTypes.string,
-      category: PropTypes.string,
-      srcolor: PropTypes.string
-  }
+    pagesize: PropTypes.string,
+    category: PropTypes.string,
+    srcolor: PropTypes.string,
+  };
 
-  constructor(){
+  constructor() {
     super();
     //created an object
     this.state = {
-          articles: [],
-          loading: false,
-          page: 1
-    }
-}
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
   // will run after render/  Async function will wait for promises to resolve
-  async componentDidMount(){
-      let apiurl = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=9d7a9b08c99a476f822b822b45b2bab5&page=1&pagesize=${this.props.pagesize}`;
-      this.setState({loading: true})
-      let data = await fetch(apiurl)
-      let parseddata = await data.json()
-      this.setState({articles: parseddata.articles, totres: parseddata.totalResults, loading: false})
-  }
-
-
-  Prevclick = async()=>{
-    let apiurl = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=9d7a9b08c99a476f822b822b45b2bab5&page=${this.state.page-1}&pagesize=${this.props.pagesize}`;
-    this.setState({loading: true})
-    let data = await fetch(apiurl)
-    let parseddata = await data.json()
+  async componentDidMount() {
+    let apiurl = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=9d7a9b08c99a476f822b822b45b2bab5&page=1&pagesize=${this.props.pagesize}`;
+    this.setState({ loading: true });
+    let data = await fetch(apiurl);
+    let parseddata = await data.json();
     this.setState({
-        loading: false,
-        page: this.state.page - 1,
-        articles: parseddata.articles
-    })
+      articles: parseddata.articles,
+      totres: parseddata.totalResults,
+      loading: false,
+    });
   }
-  Nextclick = async()=>{
-  
-      let apiurl = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=9d7a9b08c99a476f822b822b45b2bab5&page=${this.state.page+1}&pagesize=${this.props.pagesize}`;
-        this.setState({loading: true})
-        let data = await fetch(apiurl)
-        let parseddata = await data.json()
-        this.setState({
-            loading: false,
-            page: this.state.page + 1,
-            articles: parseddata.articles
-        })
-  }
+
+  Prevclick = async () => {
+    let apiurl = `https://newsapi.org/v2/top-headlines?country=in&category=${
+      this.props.category
+    }&apiKey=9d7a9b08c99a476f822b822b45b2bab5&page=${
+      this.state.page - 1
+    }&pagesize=${this.props.pagesize}`;
+    this.setState({ loading: true });
+    let data = await fetch(apiurl);
+    let parseddata = await data.json();
+    this.setState({
+      loading: false,
+      page: this.state.page - 1,
+      articles: parseddata.articles,
+    });
+  };
+  Nextclick = async () => {
+    let apiurl = `https://newsapi.org/v2/top-headlines?country=in&category=${
+      this.props.category
+    }&apiKey=9d7a9b08c99a476f822b822b45b2bab5&page=${
+      this.state.page + 1
+    }&pagesize=${this.props.pagesize}`;
+    this.setState({ loading: true });
+    let data = await fetch(apiurl);
+    let parseddata = await data.json();
+    this.setState({
+      loading: false,
+      page: this.state.page + 1,
+      articles: parseddata.articles,
+    });
+  };
 
   render() {
     return (
       <div className="container">
-        <h1 className="text-center" style={{margin: '36px'}}>Top Flash Headlines</h1>
+        <h1 className="text-center" style={{ margin: "36px" }}>
+          Top Flash Headlines
+        </h1>
 
         {/* Spinner Component */}
-        {this.state.loading && <Spinner/>}
+        {this.state.loading && <Spinner />}
 
         {/* Calling News Item in Here */}
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element)=>{
-              return  <div className="col md-3" key={element.url}>
-                <Newsitem newsurl= {element.url} title={element.title?element.title:""} description={element.description?element.description:""} imgurl={element.urlToImage?element.urlToImage:"https://st.depositphotos.com/1006899/3776/i/950/depositphotos_37765339-stock-photo-news.jpg"} author={element.author} date={element.publishedAt} source={element.source.name} srccolor={this.props.srcolor}/>
-              </div>
-          })} 
-        </div>
-        
-        {/* Prev and Next Buttons */}
-        <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page<=1} type="button" className="btn btn-success btn-lg" onClick={this.Prevclick}> &larr; Previous</button>
-          <button disabled={this.state.page > Math.ceil(this.state.totres)/this.props.pagesize} type="button" className="btn btn-success btn-lg" onClick={this.Nextclick}>Next &rarr;</button>
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col md-3" key={element.url}>
+                  <Newsitem
+                    newsurl={element.url}
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imgurl={
+                      element.urlToImage?element.urlToImage:"https://st.depositphotos.com/1006899/3776/i/950/depositphotos_37765339-stock-photo-news.jpg"
+                    }
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                    srccolor={this.props.srcolor}
+                  />
+                </div>
+              );
+            })}
         </div>
 
+        {/* Prev and Next Buttons */}
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-success btn-lg"
+            onClick={this.Prevclick}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page >
+              Math.ceil(this.state.totres) / this.props.pagesize
+            }
+            type="button"
+            className="btn btn-success btn-lg"
+            onClick={this.Nextclick}
+          >
+            Next &rarr;
+          </button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newscompo
+export default Newscompo;

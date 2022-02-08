90681e52a4316d08abd6d452d794dcd8048bfcfb
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-08 18:17:55 +0530

    Added React Router Dom and Badges

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -2,14 +2,40 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import Newscompo from './components/Newscompo';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
   // can make variables like c='Palash' and then call below using {this.c}
   render() {
     return (
       <div>
-        <Navbar/>
-        <Newscompo/>
+        <Router>
+          <Navbar/>
+          {/* You can pass country also but i dont want to */}
+            <Routes>
+            {/* Use exact if making big website so when user click / only that part opens */}
+              <Route exact path="/" element={ <Newscompo key="general" srcolor="danger" pagesize="6" category="general"/>}>
+                </Route>
+              <Route exact path="/sports" element={ <Newscompo key="sports" srcolor="warning" pagesize="6" category="sports"/>}>
+                </Route>
+              <Route exact path="/business" element={ <Newscompo key="business" srcolor="success" pagesize="6" category="business"/>}>
+                </Route>
+              <Route exact path="/technology" element={ <Newscompo key="technology" srcolor="dark" pagesize="6" category="technology"/>}>
+                </Route>
+              <Route exact path="/general" element={ <Newscompo key="general" srcolor="danger" pagesize="6" category="general"/>}>
+                </Route>
+              <Route exact path="/health" element={ <Newscompo key="health" srcolor="info" pagesize="6" category="health"/>}>
+                </Route>
+              <Route exact path="/entertainment" element={ <Newscompo key="entertainment" srcolor="secondary" pagesize="6" category="entertainment"/>}>
+                </Route>
+              <Route exact path="/science" element={ <Newscompo key="science" srcolor="primary" pagesize="6" category="science"/>}>
+                </Route>
+          </Routes>
+        </Router>
       </div>
     )
   }
--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 export class Newsitem extends Component {
   render() {
     // when props called title and description will be assigned those value
-    let {title, description, imgurl, newsurl} = this.props
+    let {title, description, imgurl, newsurl, author, date, source, srccolor} = this.props
 
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{width: "21rem"}}>
           <img src={imgurl} className="card-img-top" alt="..." />
           <div className="card-body">
+            <h5><span className={`position-absolute top-0 translate-middle badge rounded-pill bg-${srccolor}`} style={{left: '90%', zIndex: '1'}}>{source}</span></h5>
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {author?author:"Anonymous"} On {new Date(date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsurl} className="btn btn-m btn-outline-success" target="_blank">Read More</a>
           </div>
         </div>
